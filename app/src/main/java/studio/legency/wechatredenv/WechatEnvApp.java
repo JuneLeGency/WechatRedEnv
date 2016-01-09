ae2576029ce9b5a1056ff19b2a0e341ab60fcae9
legency <lichen900210@gmail.com>
2016-01-09 18:12:40 +0800

    集成 fir.im

--- app/src/main/java/studio/legency/wechatredenv/WechatEnvApp.java
+++ app/src/main/java/studio/legency/wechatredenv/WechatEnvApp.java
@@ -1,13 +1,49 @@
 package studio.legency.wechatredenv;
 
+import android.util.Log;
+import android.widget.Toast;
+
 import com.orm.SugarApp;
 
 import org.androidannotations.annotations.EApplication;
+
+import im.fir.sdk.FIR;
+import im.fir.sdk.VersionCheckCallback;
+import io.paperdb.Paper;
 
 /**
  * Created by Administrator on 2015/9/30.
  */
 @EApplication
 public class WechatEnvApp extends SugarApp {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Paper.init(this);
+        FIR.init(this);
+        FIR.checkForUpdateInFIR("acc0748415b87fa329d4c149f3a01a04", new VersionCheckCallback() {
+            @Override
+            public void onSuccess(String versionJson) {
+                Log.i("fir", "check from fir.im success! " + "\n" + versionJson);
+            }
+
+            @Override
+            public void onFail(Exception exception) {
+                Log.i("fir", "check fir.im fail! " + "\n" + exception.getMessage());
+            }
+
+            @Override
+            public void onStart() {
+                Toast.makeText(getApplicationContext(), "正在获取", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onFinish() {
+                Toast.makeText(getApplicationContext(), "获取完成", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
 
 }
