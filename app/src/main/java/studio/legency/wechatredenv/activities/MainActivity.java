183ed4edaefa3b73a7ca342a633d0ac662be97ed
legency <lichen900210@gmail.com>
2016-01-29 18:57:07 +0800

    more changes

--- app/src/main/java/studio/legency/wechatredenv/activities/MainActivity.java
+++ app/src/main/java/studio/legency/wechatredenv/activities/MainActivity.java
@@ -1,8 +1,14 @@
 package studio.legency.wechatredenv.activities;
 
+import android.annotation.TargetApi;
 import android.app.Activity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.TextView;
+
+import com.readystatesoftware.systembartint.SystemBarTintManager;
 
 import org.androidannotations.annotations.AfterViews;
 import org.androidannotations.annotations.Click;
@@ -20,6 +26,31 @@ import studio.legency.wechatredenv.helpers.AccessibilityServiceHelper;
  */
 @EActivity(R.layout.activity_main)
 public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        SystemBarTintManager tintManager = new SystemBarTintManager(this);
+        // enable status bar tint
+        tintManager.setStatusBarTintEnabled(true);
+        // enable navigation bar tint
+        tintManager.setNavigationBarTintEnabled(true);
+
+        setTranslucentStatus(true);
+    }
+
+    @TargetApi(19)
+    private void setTranslucentStatus(boolean on) {
+        Window win = getWindow();
+        WindowManager.LayoutParams winParams = win.getAttributes();
+        final int bits = WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS;
+        if (on) {
+            winParams.flags |= bits;
+        } else {
+            winParams.flags &= ~bits;
+        }
+        win.setAttributes(winParams);
+    }
 
     @ViewById
     TextView service_state;
@@ -63,7 +94,7 @@ public class MainActivity extends Activity {
     }
 
     @Click
-    void clear_data(){
+    void clear_data() {
         WechatRedEnvHis.deleteAll(WechatRedEnvHis.class);
     }
 }
--- app/src/main/java/studio/legency/wechatredenv/services/AccessService.java
+++ app/src/main/java/studio/legency/wechatredenv/services/AccessService.java
@@ -4,10 +4,16 @@ import android.accessibilityservice.AccessibilityService;
 import android.accessibilityservice.AccessibilityServiceInfo;
 import android.annotation.TargetApi;
 import android.content.Context;
+import android.graphics.Color;
+import android.graphics.PixelFormat;
+import android.graphics.Rect;
 import android.os.Build;
 import android.os.PowerManager;
+import android.view.View;
+import android.view.WindowManager;
 import android.view.accessibility.AccessibilityEvent;
 import android.view.accessibility.AccessibilityNodeInfo;
+import android.widget.RelativeLayout;
 
 import com.apkfuns.logutils.LogUtils;
 
@@ -36,6 +42,8 @@ public class AccessService extends AccessibilityService {
 
     private PowerManager.WakeLock lock;
 
+    private RelativeLayout relativeLayout;
+
     @Override
     protected void onServiceConnected() {
         LogUtils.d("微信服务已连接");
@@ -63,11 +71,11 @@ public class AccessService extends AccessibilityService {
         AccessibilityServiceInfo accessibilityServiceInfo = getServiceInfo();
         if (accessibilityServiceInfo == null)
             accessibilityServiceInfo = new AccessibilityServiceInfo();
-//        accessibilityServiceInfo.eventTypes = AccessibilityEvent.TYPES_ALL_MASK;
-        accessibilityServiceInfo.eventTypes = AccessibilityEvent.TYPE_WINDOW_CONTENT_CHANGED |
-                AccessibilityEvent.TYPE_NOTIFICATION_STATE_CHANGED |
-                AccessibilityEvent.TYPE_WINDOW_STATE_CHANGED | AccessibilityEvent.TYPE_VIEW_LONG_CLICKED |
-                AccessibilityEvent.TYPE_VIEW_CLICKED;
+        accessibilityServiceInfo.eventTypes = AccessibilityEvent.TYPES_ALL_MASK;
+//        accessibilityServiceInfo.eventTypes = AccessibilityEvent.TYPE_WINDOW_CONTENT_CHANGED |
+//                AccessibilityEvent.TYPE_NOTIFICATION_STATE_CHANGED |
+//                AccessibilityEvent.TYPE_WINDOW_STATE_CHANGED | AccessibilityEvent.TYPE_VIEW_LONG_CLICKED |
+//                AccessibilityEvent.TYPE_VIEW_CLICKED;
         accessibilityServiceInfo.flags |= AccessibilityServiceInfo.FLAG_REPORT_VIEW_IDS;
         accessibilityServiceInfo.packageNames = new String[]{WechatInfo.package_name, DingInfo.package_name};
         accessibilityServiceInfo.feedbackType = AccessibilityServiceInfo.FEEDBACK_ALL_MASK;
@@ -92,11 +100,13 @@ public class AccessService extends AccessibilityService {
             }
 
             if (Common.is_view_test()) {
-                if (event.getEventType() == AccessibilityEvent.TYPE_VIEW_LONG_CLICKED ||
-                        event.getEventType() == AccessibilityEvent.TYPE_VIEW_CLICKED) {
+                int[] types = {AccessibilityEvent.TYPE_VIEW_LONG_CLICKED, AccessibilityEvent.TYPE_VIEW_CLICKED, AccessibilityEvent.TYPE_TOUCH_INTERACTION_START};
+//                int[] types = {AccessibilityEvent.TYPE_TOUCH_INTERACTION_START};
+                if (inType(types, event.getEventType())) {
                     AccessibilityNodeInfo nodeInfo = event.getSource();
                     if (nodeInfo == null) return;
                     nodeFinder.debugNode(nodeInfo);
+                    showWindow(nodeInfo.getParent().getParent());
                 }
                 return;
             }
@@ -109,7 +119,66 @@ public class AccessService extends AccessibilityService {
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
 
+    boolean inType(int[] types, int type) {
+        for (int type1 : types) {
+            if (type == type1) return true;
+        }
+        return false;
+    }
+
+    private void showWindow(AccessibilityNodeInfo nodeInfo) {
+        if (nodeInfo == null) return;
+        int type = WindowManager.LayoutParams.TYPE_SYSTEM_OVERLAY;
+//                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+//                        type = WindowManager.LayoutParams.TYPE_TOAST;
+//                    } else {
+//                        type = WindowManager.LayoutParams.TYPE_PHONE;
+//                    }
+//                    Log.d("ttttt", type + "asd");
+        if (relativeLayout == null) {
+            WindowManager.LayoutParams params = new WindowManager.LayoutParams(
+                    WindowManager.LayoutParams.MATCH_PARENT, WindowManager.LayoutParams.MATCH_PARENT,
+                    type
+                    ,
+                    WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE, PixelFormat.TRANSLUCENT);
+            params.flags |= WindowManager.LayoutParams.FLAG_LAYOUT_IN_SCREEN;
+            WindowManager wm = (WindowManager) getSystemService(WINDOW_SERVICE);
+            relativeLayout = new RelativeLayout(this);
+            relativeLayout.setBackgroundColor(Color.argb(55, 200, 100, 100));
+            wm.addView(relativeLayout, params);
+        } else {
+            relativeLayout.removeAllViews();
+        }
+        addViewToLayout(nodeInfo);
+    }
+
+    public void addViewToLayout(AccessibilityNodeInfo info) {
+        if (info == null) return;
+        addView(info, relativeLayout);
+        if (info.getChildCount() != 0) {
+            for (int i = 0; i < info.getChildCount(); i++) {
+                if (info.getChild(i) != null) {
+                    addViewToLayout(info.getChild(i));
+                }
+            }
+        }
+    }
+
+    private void addView(AccessibilityNodeInfo info, RelativeLayout relativeLayout) {
+        Rect rect = new Rect();
+        info.getBoundsInScreen(rect);
+        int w = rect.right - rect.left;
+        int h = rect.bottom - rect.top;
+        View view = new View(this);
+        relativeLayout.addView(view);
+        RelativeLayout.LayoutParams l = (RelativeLayout.LayoutParams) view.getLayoutParams();
+        l.leftMargin = rect.left;
+        l.topMargin = rect.top;
+        l.width = w;
+        l.height = h;
+        view.setBackgroundColor(Color.argb(55, 100, 100, 200));
     }
 
     @Override
